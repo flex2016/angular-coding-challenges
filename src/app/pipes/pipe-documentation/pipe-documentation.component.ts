c4169dcb97e93f3757bb6cd67f0a26f00fc19243
taras <reloded2004@hotmail.com>
2024-06-22 09:27:28 -0400

    prebuilt pipes

--- src/app/pipes/pipe-documentation/pipe-documentation.component.ts
+++ src/app/pipes/pipe-documentation/pipe-documentation.component.ts
@@ -10,4 +10,13 @@ export class PipeDocumentationComponent {
     'This is a long text that needs to be truncated because it exceeds the default maximum length of 100 characters.';
   nullText: string | null = null;
   undefinedText: string | undefined = undefined;
+
+  fee: number = 0.031234;
+  amount: number = 123.45;
+  currency: string = 'EUR';
+  timeOftransaction: Date = this.getPresentDate();
+
+  public getPresentDate() {
+    return new Date(1997, 6, 1, 12, 32);
+  }
 }
